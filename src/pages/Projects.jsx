ce84f5eb67faab7adec21ce108a535d7c5dc05ac
David Mott <d.s.mott0808@gmail.com>
2022-10-07 10:25:17 -0700

    update projects

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -3,6 +3,7 @@ import gitHubFinderIMG from '../components/assets/gitHubFinder.png'
 import feedbackAppIMG from '../components/assets/feedbackApp.png'
 import hsoIMG from '../components/assets/hsoApp.png'
 import mernIMG from '../components/assets/mernApp.png'
+import marketplaceIMG from '../components/assets/marketplaceApp.png'
 
 const Projects = () => {
   const gitFinderExplanation =
@@ -16,6 +17,9 @@ const Projects = () => {
 
   const mernExplanation =
     'This is a full stack app for setting and saving goals.'
+
+  const marketplaceExplanation =
+    'This is a React app to upload and view house listings.'
   return (
     <div className='grid justify-items-center grid-cols-1 gap-6 2xl:grid-cols-4  xl:grid-cols-3 xl:gap-24  lg:grid-cols-2   md:grid-cols-2 md:gap-12 sm:grid-cols-1'>
       <ProjectCard
@@ -45,6 +49,13 @@ const Projects = () => {
         repoLink='https://github.com/Hi-ImDave/mern-app'
         webLink='https://mernappdsm.herokuapp.com/register'
         projectImg={mernIMG}
+      />
+      <ProjectCard
+        title='House Marketplace App'
+        explanation={marketplaceExplanation}
+        repoLink='https://github.com/Hi-ImDave/house-marketplace-app'
+        webLink='https://github.com/Hi-ImDave/house-marketplace-app'
+        projectImg={marketplaceIMG}
       />
     </div>
   )
